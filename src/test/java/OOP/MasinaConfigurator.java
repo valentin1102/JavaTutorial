8557917391f1939e2535c6b3902b4e09522d2b44
Valentin <valentin.stoica90@yahoo.ro>
2024-07-09 20:40:49 +0300

    am facut primul OOP

---
+++ src/test/java/OOP/MasinaConfigurator.java
@@ -0,0 +1,21 @@
+package OOP;
+
+public class MasiniConfigurator {
+
+    public String marca;
+    public String model;
+    public Integer an;
+    public Double pretStandard;
+
+    public MasiniConfigurator(String marca, String model, Integer an, Double pretStandard) {
+        this.marca = marca;
+        this.model = model;
+        this.an = an;
+        this.pretStandard = pretStandard;
+    }
+
+    //OOP= programare orientata pe obiecte
+    //OOP = 4 principii -> mostenire, incapsulare, polimorfism, abstractizare
+    //Mostenirea:
+
+}
